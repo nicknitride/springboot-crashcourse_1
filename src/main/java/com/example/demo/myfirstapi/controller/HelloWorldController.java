2420503cb42585cde25cb42feaae104bd763e610
Nicholo Patrick Pardines <nick123abl@gmail.com>
2025-07-14 14:53:01 +0800

    Challenge Lab 2

--- src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
@@ -3,6 +3,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.web.bind.annotation.RequestParam; //New import, challenge lab 2,
 @RestController // Marks this class as a REST controller
 public class HelloWorldController {
     @Value("${app.greeting.message}")
@@ -16,5 +18,14 @@ public class HelloWorldController {
     @GetMapping("/greet/{name}") // Path now matches and expects /greet/<name>, name is dynamic
     public String greetPerson(@PathVariable String name){
         return String.format("Hello %s! \n",name);
+    }
+
+    public String greetWithParam(
+            @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
+    }
+    @GetMapping("/calculate/sum")
+    public String sumNumbers( @RequestParam("num1") int number1, @RequestParam("num2") int number2) {
+        int sum = number1 + number2; return "The sum of " + number1 + " and " + number2 + " is " + sum;
     }
 }
