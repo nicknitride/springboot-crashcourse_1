88c39cb7cfde5c64724f6c52862a90035a7460ad
Nicholo Patrick Pardines <nick123abl@gmail.com>
2025-07-14 14:49:50 +0800

    Init commit

---
+++ src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com. example.demo.myfirstapi.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+@RestController // Marks this class as a REST controller
+public class HelloWorldController {
+    @Value("${app.greeting.message}")
+    private String customGreeting;
+
+    @GetMapping("/hello") // Maps HTTP GET requests to the "/hello" URL
+    public String sayHello() {
+        return "Hello, Spring Boot Learners!\n"+customGreeting;
+    }
+
+    @GetMapping("/greet/{name}") // Path now matches and expects /greet/<name>, name is dynamic
+    public String greetPerson(@PathVariable String name){
+        return String.format("Hello %s! \n",name);
+    }
+}
