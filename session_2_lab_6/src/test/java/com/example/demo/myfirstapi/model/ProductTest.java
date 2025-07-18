930ca1e0f21275e45a63494cb9669f245c6e5fad
Jaime Manalo + Nicholo Pardines <jaime.manalo+nicholo.pardines@gcash.com>
2025-07-18 14:09:31 +0800

    Added skeleton of ProductTest.java

---
+++ session_2_lab_6/src/test/java/com/example/demo/myfirstapi/model/ProductTest.java
@@ -0,0 +1,44 @@
+package com.example.demo.myfirstapi.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
