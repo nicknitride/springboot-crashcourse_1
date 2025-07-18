0369ca7edee0a905636cb32b4e0212ad2e4d7ab6
Jaime Manalo + Nicholo Pardines <jaime.manalo+nicholo.pardines@gcash.com>
2025-07-18 11:38:20 +0800

    Added sum tests

--- session2/calculator-tdd-style/src/test/java/org/ZooFloresManaloPardines/CalculatorTest.java
+++ session2/calculator-tdd-style/src/test/java/org/ZooFloresManaloPardines/CalculatorTest.java
@@ -1,9 +1,50 @@
 package org.ZooFloresManaloPardines;
 
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calc;
+    @BeforeEach
+    void setup(){
+        calc = new Calculator();
+    }
+
+    @Test
+    public void testSumInt(){
+        int actual = calc.sum(10,2);
+        Assertions.assertEquals(12,actual);
+    }
+
+    @Test
+    public void testSumDouble(){
+        double actual = this.calc.sum(10.0d,2.0d);
+        Assertions.assertEquals(12d,actual, 0.0001);
+    }
+
+    @Test
+    public void testSumFloat(){
+        float actual = this.calc.sum(10.0f,2.0f);
+        Assertions.assertEquals(12f, actual, 0.0001);
+    }
+
+    @Test
+    public void testSumShort(){
+        short actual = (short) this.calc.sum(10,2);
+        Assertions.assertEquals(12,actual, 0.0001);
+    }
+
+    @Test
+    public void testSumLong(){
+        long actual = this.calc.sum(10,2);
+        Assertions.assertEquals(12,actual, 0.0001);
+    }
+
+
+
+
+}
 
 
