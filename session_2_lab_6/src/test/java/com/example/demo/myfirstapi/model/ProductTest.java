e1f3cd1afd90326f8f0f8ebd596774bcbf5c053d
Nicholo Pardines <nicholo.pardines@gcash.com>
2025-07-18 14:14:49 +0800

    Added skeleton of ProductTest.java

--- session_2_lab_6/src/test/java/com/example/demo/myfirstapi/model/ProductTest.java
+++ session_2_lab_6/src/test/java/com/example/demo/myfirstapi/model/ProductTest.java
@@ -1,25 +1,36 @@
 package com.example.demo.myfirstapi.model;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
-
+    Product prod;
+    @BeforeEach
+    void setupProduct(){
+        this.prod = new Product(23L,"Fresh Milk",34.5);
+    }
     @Test
     void getId() {
+        Assertions.assertEquals("Fresh Milk",this.prod.getId());
     }
 
     @Test
     void getName() {
+        Assertions.assertEquals("Fresh Milk",this.prod.getName());
+
     }
 
     @Test
     void getPrice() {
+        Assertions.assertEquals(34.5,this.prod.getPrice());
     }
 
     @Test
     void setId() {
+
     }
 
     @Test
