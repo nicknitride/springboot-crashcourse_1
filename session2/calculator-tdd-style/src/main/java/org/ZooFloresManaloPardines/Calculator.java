c82a42621c58c7317fbf1b2e3a9bc3e63f66587d
Jaime Manalo + Nicholo Pardines <jaime.manalo+nicholo.pardines@gcash.com>
2025-07-18 11:48:00 +0800

    Added main calculator class and methods

---
+++ session2/calculator-tdd-style/src/main/java/org/ZooFloresManaloPardines/Calculator.java
@@ -0,0 +1,61 @@
+package org.ZooFloresManaloPardines;
+
+public class Calculator {
+    public int sum(int a, int b){
+        return a+b;
+    }
+    public double sum(double a, double b){
+        return a+b;
+    }
+    public float sum(float a, float b){
+        return a+b;
+    }
+
+    public double quotient(double a,double b){
+        try{
+            return a/b;
+        }catch (ArithmeticException e){
+            System.err.println("Cannot divide by zero"+e.getMessage());
+        }
+
+        return 0;
+    }
+
+    public float quotient(float a,float b){
+        try{
+            return a/b;
+        }catch (ArithmeticException e){
+            System.err.println("Cannot divide by zero"+e.getMessage());
+        }
+        return 0;
+    }
+
+    public int quotient(int a,int b){
+        try{
+            return a/b;
+        }catch (ArithmeticException e){
+            System.err.println("Cannot divide by zero"+e.getMessage());
+        }
+        return 0;
+    }
+
+    public int product(int a, int b){
+        return a*b;
+    }
+    public float product(float a, float b){
+        return a*b;
+    }
+    public double product(double a, double b){
+        return a*b;
+    }
+    public int difference(int a, int b){
+        return (a-b);
+    }
+    public double difference(double a, double b){
+        return (a-b);
+    }
+    public float difference(float a, float b){
+        return (a-b);
+    }
+
+}
