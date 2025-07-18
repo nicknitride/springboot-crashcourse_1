3547318855a8174618fa8c84a5bed27320f3ab74
Nicholo Pardines <nicholo.pardines@gcash.com>
2025-07-18 14:30:29 +0800

    Completed Product unit tests

--- session_2_lab_6/src/test/java/com/example/demo/myfirstapi/model/ProductTest.java
+++ session_2_lab_6/src/test/java/com/example/demo/myfirstapi/model/ProductTest.java
@@ -1,8 +1,12 @@
 package com.example.demo.myfirstapi.model;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,42 +18,53 @@ class ProductTest {
     }
     @Test
     void getId() {
-        Assertions.assertEquals("Fresh Milk",this.prod.getId());
+        Assertions.assertEquals(23L,this.prod.getId());
     }
-
+    @Test
+    void setID(){
+        this.prod.setId(200L);
+        Assertions.assertEquals(200L,this.prod.getId());
+    }
     @Test
     void getName() {
         Assertions.assertEquals("Fresh Milk",this.prod.getName());
-
+    }
+    @Test
+    void setName(){
+        this.prod.setName("AHHH");
+        Assertions.assertEquals("AHHH",this.prod.getName());
     }
 
+    @DisplayName("Get Price")
     @Test
-    void getPrice() {
+    void getPrice(){
         Assertions.assertEquals(34.5,this.prod.getPrice());
     }
-
     @Test
-    void setId() {
-
+    void setPrice(){
+        this.prod.setPrice(212000d);
+        Assertions.assertEquals(212000d,this.prod.getPrice());
     }
-
-    @Test
-    void setName() {
-    }
-
-    @Test
-    void setPrice() {
-    }
-
     @Test
     void testToString() {
+        long id = 23L;
+        String name = "Fresh Milk";
+        double price = 34.5d;
+        String result =  "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+        Assertions.assertEquals(result,this.prod.toString());
     }
 
     @Test
     void testEquals() {
+        assertTrue(prod.equals(prod));
     }
 
     @Test
     void testHashCode() {
+        assertEquals(Objects.hash(23L,"Fresh Milk",34.5),this.prod.hashCode());
     }
 }
